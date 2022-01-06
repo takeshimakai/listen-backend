0b3c26a4693902c3cc64dd4f0533c410ce7c1bf8
Kai Takeshima <takeshimakai@gmail.com>
2022-01-06 12:27:04 -0500

    mongoDB config

--- config/mongoDB.js
+++ config/mongoDB.js
@@ -3,8 +3,7 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const mongoDB = process.env.MONGODB_URI;
-mongoose.connect(mongoDB, { useNewUrlParser: true , useUnifiedTopology: true});
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true , useUnifiedTopology: true});
 mongoose.set('useFindAndModify', false);
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
